08973e233ebf43da777272e21707bcb10dcad3d1
Matthieu Viry <matthieu.viry@cnrs.fr>
2024-01-10 17:47:59 +0100

    Allow to disable layer creation validation button if needed

--- src/components/Inputs/InputButtonValidation.tsx
+++ src/components/Inputs/InputButtonValidation.tsx
@@ -4,12 +4,14 @@ import { type LocalizedString } from 'typesafe-i18n';
 interface ButtonValidationProps {
   label: LocalizedString | string;
   onClick: () => void;
+  disabled?: boolean;
 }
 export default function ButtonValidation(props: ButtonValidationProps): JSX.Element {
   return <div class="has-text-centered">
     <button
       class="button is-success portrayal-section__button-validation"
       onClick={props.onClick}
+      disabled={props.disabled}
     >
       { props.label }
     </button>
