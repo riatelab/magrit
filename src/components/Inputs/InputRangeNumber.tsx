334828c0b01f7bc6c960951a6ff3d633d484cf36
Matthieu Viry <matthieu.viry@cnrs.fr>
2024-02-09 17:34:07 +0100

    Input component with synchronised inputs range and number

---
+++ src/components/Inputs/InputRangeNumber.tsx
@@ -0,0 +1,49 @@
+import { createEffect, createSignal, JSX } from 'solid-js';
+
+interface InputFieldRangeNumberProps {
+  label: string;
+  // formater?: (value: number) => string;
+  value: number;
+  onChange: (number: number) => void;
+  min: number;
+  max: number;
+  step: number;
+}
+
+export default function InputFieldRangeNumber(props: InputFieldRangeNumberProps): JSX.Element {
+  const [value, setValue] = createSignal(props.value);
+
+  createEffect(() => {
+    setValue(props.value);
+  });
+
+  return <div class="field">
+    <label class="label">{props.label}</label>
+    <div class="control">
+      <input
+        class="range"
+        type="range"
+        onChange={(e) => {
+          props.onChange(+e.currentTarget.value);
+          setValue(+e.currentTarget.value);
+        }}
+        value={value()}
+        min={props.min}
+        max={props.max}
+        step={props.step}
+      />
+      <input
+        class="input"
+        type="number"
+        value={value()}
+        min={props.min}
+        max={props.max}
+        step={props.step}
+        onChange={(e) => {
+          props.onChange(+e.currentTarget.value);
+          setValue(+e.currentTarget.value);
+        }}
+      />
+    </div>
+  </div>;
+}
