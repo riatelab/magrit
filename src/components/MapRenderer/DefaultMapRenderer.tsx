0eeff4a9a1e9a5bb428e085c032f7467ff30c6dc
Matthieu Viry <matthieu.viry@cnrs.fr>
2024-04-26 12:52:53 +0200

    Use same logic as in other function for shape rendering property

--- src/components/MapRenderer/DefaultMapRenderer.tsx
+++ src/components/MapRenderer/DefaultMapRenderer.tsx
@@ -39,7 +39,11 @@ export function defaultPolygonRenderer(
     stroke-linejoin="round"
     clip-path="url(#clip-sphere)"
     filter={mergeFilterIds(layerDescription)}
-    shape-rendering={layerDescription.strokeWidth === 0 ? 'crispEdges' : layerDescription.shapeRendering}
+    shape-rendering={
+      (layerDescription.strokeWidth === 0 || layerDescription.strokeOpacity === 0)
+        ? 'crispEdges'
+        : layerDescription.shapeRendering
+    }
     mgt:geometry-type={layerDescription.type}
     mgt:portrayal-type={layerDescription.renderer}
   >
