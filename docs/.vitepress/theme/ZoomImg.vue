70a03baf650b03f56e18f5198cea9e6870bdf8c8
Matthieu Viry <matthieu.viry@cnrs.fr>
2024-06-09 18:28:06 +0200

    docs: add ZoomImg component to allow to click and zoom on images

---
+++ docs/.vitepress/theme/ZoomImg.vue
@@ -0,0 +1,38 @@
+<script setup>
+  import { withBase } from 'vitepress';
+  import { onMounted, ref } from 'vue';
+  import mediumZoom from 'medium-zoom';
+
+  defineProps({
+    src: String,
+    alt: String,
+    caption: String,
+  });
+
+  const imgRef = ref(null);
+
+  onMounted(() => {
+    mediumZoom(
+        imgRef.value,
+        { background: 'var(--vp-c-bg)' },
+    );
+  });
+</script>
+
+<template>
+  <div style="text-align: center;">
+    <br>
+    <img ref="imgRef" :src="withBase(src)" :alt="alt" style="margin: auto;" />
+    <i>{{ caption }}</i>
+  </div>
+</template>
+
+<style>
+.medium-zoom-overlay {
+  z-index: 1000;
+  opacity: 0.75 !important;
+}
+.medium-zoom-image {
+  z-index: 1001;
+}
+</style>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,4 +1,11 @@
 import DefaultTheme from 'vitepress/theme';
+import ZoomImg from './ZoomImg.vue';
+
 import './custom.css';
 
-export default DefaultTheme;
+export default {
+  ...DefaultTheme,
+  enhanceApp({ app }) {
+    app.component('ZoomImg', ZoomImg)
+  },
+};
