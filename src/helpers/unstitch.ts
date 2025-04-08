e0b28c18f83d1b4818b8e1a81a0889d0d5c6899a
Matthieu Viry <matthieu.viry@cnrs.fr>
2025-04-08 11:07:45 +0200

    Add unstich helper function

---
+++ src/helpers/unstitch.ts
@@ -0,0 +1,38 @@
+import d3 from './d3-custom';
+import rewindLayer from './rewind';
+import { GeoJSONFeatureCollection } from '../global';
+
+// From https://github.com/d3/d3-geo/issues/113
+export default function unstitch(a: GeoJSONFeatureCollection) {
+  let d = JSON.parse(JSON.stringify(a)) as GeoJSONFeatureCollection;
+  d = d3.geoProject(
+    d,
+    d3
+      .geoEquirectangular()
+      .scale(180 / Math.PI)
+      .translate([0, 0]),
+  );
+
+  d.features.forEach((feature) => {
+    const f = feature.geometry;
+    if (f.type === 'Polygon') {
+      f.coordinates.forEach((ring) => {
+        ring.forEach((point) => {
+          // eslint-disable-next-line no-param-reassign
+          point[1] *= -1;
+        });
+      });
+    } else if (f.type === 'MultiPolygon') {
+      f.coordinates.forEach((poly) => {
+        poly.forEach((ring) => {
+          ring.forEach((point) => {
+            // eslint-disable-next-line no-param-reassign
+            point[1] *= -1;
+          });
+        });
+      });
+    }
+  });
+
+  return rewindLayer(a);
+}
